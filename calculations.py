8f57105605e283fc9e796fa9ad55a857a893f9b5
pierretoussing <pierre@toussing.lu>
2020-06-04 16:46:11 +0200

    Add min-max noramlization
    Scale value ranges to [a, b] to make them comparable

--- calculations.py
+++ calculations.py
@@ -297,13 +297,33 @@ def equalize_histogram(map_array, num_bins=10):
     value range of the map.
 
     Args:
-        map_array (array): Map with values to scale
+        map_array (np.ndarray): Map with values to scale
         num_bins (int): Number or bins
 
     Returns:
         Map with the scaled values
     """
     return exposure.equalize_hist(map_array, nbins=num_bins)
+
+
+def min_max_normalization(map_array, a=0, b=1):
+    """
+    Rescale a map of values ro range [a, b] using min-max normalization
+
+    Args:
+        map_array (np.ndarray): Map with values to scale
+        a (int, optional): Lower bound
+            Defaults to 0
+        b (int, optional): Upper bound
+            Defaults to 0
+
+    Returns:
+        Map with scaled values
+    """
+    min = map_array.min()
+    max = map_array.max()
+
+    return a + ((map_array - min) * (b - a) / (max - min))
 
 
 def combine_similarity_metrics(similarities_1, similarities_2, combination_func):
